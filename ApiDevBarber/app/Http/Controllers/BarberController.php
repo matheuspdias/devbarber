b29aa00c02433c7ca8e935a448086d4236ca1cce
Matheus Pereira <matheus.tecinfor98@gmail.com>
2020-09-16 23:42:44 -0300

    Mobile - pegando informações do usuário

--- ApiDevBarber/app/Http/Controllers/BarberController.php
+++ ApiDevBarber/app/Http/Controllers/BarberController.php
@@ -3,8 +3,96 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+use App\Models\Barber;
+use App\Models\BarberPhoto;
+use App\Models\BarberService;
+use App\Models\BarberTestimonial;
+use App\Models\BarberAvailability;
+
 
 class BarberController extends Controller
-{
-    //
+{   
+    private $loggedUser;
+
+    public function __construct() {
+        $this->middleware('auth:api');
+        $this->loggedUser = auth()->user();
+    }
+
+    public function createRandom() {
+        $array = ['error' => ''];
+
+        for($q=0;$q<15;$q++) {
+            $names = ['Bonieky', 'Matheus', 'Lucas', 'Pedro', 'Daniel', 'Amanda', 'Leticia', 'Gabriel', 'Marcos'];
+            $lastnames = ['Pereira', 'Silva', 'Lacerda', 'Diniz', 'Alvaro', 'Sousa', 'Oliveira', 'Dantes'];
+
+            $servicos = ['Corte', 'Pintura', 'Aparação', 'Enfeite'];
+            $servicos2 = ['Cabelo', 'Unha', 'Sobrancelhas', 'Fios'];
+
+            $depos = [
+                'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years',
+                'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humou',
+                'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
+                'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+                'All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary'
+            ];
+
+            $newBarber = new Barber();
+            $newBarber->name = $names[rand(0, count($names)-1)].' '.$lastnames[rand(0, count($lastnames)-1)];
+            $newBarber->avatar = rand(1, 4).'.png';
+            $newBarber->stars = rand(2, 4).'.'.rand(0,9);
+            $newBarber->latitude = '-23.5'.rand(0,9).'30907';
+            $newBarber->longitude = '-46.6'.rand(0, 9).'82795';
+            $newBarber->save();
+
+            $ns = rand(3, 6);
+
+            for($w=0;$w<4;$w++) {
+                $newBarberPhoto = new BarberPhoto();
+                $newBarberPhoto->id_barber = $newBarber->id;
+                $newBarberPhoto->url = rand(1, 5).'.png';
+                $newBarberPhoto->save();
+            }
+
+            for($w=0;$w<$ns;$w++) {
+                $newBarberService = new BarberService();
+                $newBarberService->id_barber = $newBarber->id;
+                $newBarberService->name = $servicos[rand(0, count($servicos)-1)].' de '.$servicos2[rand(0, count($servicos2)-1)];
+                $newBarberService->price = rand(1, 99).'.'.rand(0, 100);
+                $newBarberService->save();
+            }
+
+            for($w=0;$w<3;$w++) {
+                $newBarberTestimonial = new BarberTestimonial();
+                $newBarberTestimonial->id_barber = $newBarber->id;
+                $newBarberTestimonial->name = $names[rand(0, count($names)-1)];
+                $newBarberTestimonial->rate = rand(2, 4).'.'.rand(0, 9);
+                $newBarberTestimonial->body = $depos[rand(0, count($depos)-1)];
+                $newBarberTestimonial->save();    
+            }
+
+            for($e=0;$e<4;$e++) {
+                $rAdd = rand(7, 10);
+                $hours = [];
+                for($r=0;$r<8;$r++) {
+                    $time = $r + $rAdd;
+                    if($time < 10) {
+                        $time = '0'.$time;
+                    }
+                    $hours[] = $time.':00';
+                }
+                $newBarberAvail = new BarberAvailability();
+                $newBarberAvail->id_barber = $newBarber->id;
+                $newBarberAvail->weekday = $e;
+                $newBarberAvail->hours = implode(',', $hours);
+                $newBarberAvail->save();
+            }
+        }
+        
+        return $array;
+    }
+    
 }
